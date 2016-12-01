19b71122d04308135a6c96694d84ef6febdce491
Vivek Gupta <vivekgupta3.vg@gmail.com>
2016-12-01 11:15:18 +0530

    added a space

--- p1.py
+++ p1.py
@@ -9,7 +9,7 @@ while((i+n)<=len(str1)):	#loop through all possible substrings of length n in st
         j=str2.find(s,k,len(str2))		#check if a substring in str1 is present in str2
         if j!=-1:
             print(s+" "+str(i+1)+" "+str(j+1))
-            k=j+1						#for multiple occurences of a substring in str2
+            k=j+1				#for multiple occurences of a substring in str2
         else:
             break                
     i+=1
